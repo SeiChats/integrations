a1ff5ee2bf1c87ba8d3513f0ec5ec28946df900d
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-05-15 22:18:19 +0100

    feat: add optional image to password input label

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,26 +1,31 @@
 import { EyeIcon } from './icons'
 
-import { ComponentPropsWithoutRef, useState } from 'react'
+import { ComponentPropsWithoutRef, ReactNode, useState } from 'react'
 
 interface PasswordInputProps {
   label: string
   htmlFor: string
+  children?: ReactNode
 }
 
 const PasswordInput = function ({
   label,
   htmlFor,
+  children,
   ...inputProps
 }: PasswordInputProps & ComponentPropsWithoutRef<'input'>) {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
   return (
     <label htmlFor={htmlFor} className="capitalize block relative mb-6">
-      {label}
+      <div className="flex gap-2 items-center">
+        {label}
+        {children}
+      </div>
       <input
         type={isPasswordVisible ? 'text' : 'password'}
         name={htmlFor}
         {...inputProps}
-        className="bg-transparent border border-white/20 shadow-custom mt-4 pl-4 pr-12 py-3 rounded-xl w-full"
+        className="bg-transparent border border-white/20 shadow-custom mt-4 pl-4 pr-12 py-3 rounded-xl w-full disabled:cursor-not-allowed"
       />
       <EyeIcon
         visible={isPasswordVisible}
