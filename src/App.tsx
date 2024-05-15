13f4679a9cd2b1cc2152a23165010a12f1095f07
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-05-15 13:50:07 +0100

    feat: disable password input and button while wallet connects

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 // import Loading from './pages/Loading'
 
 import { useContext } from 'react'
+import { motion } from 'framer-motion'
 
 import MessagesLayout from './layouts/MessagesLayout'
 import RouteContext from './ContextProvider'
@@ -12,13 +13,19 @@ function App() {
   const { route } = useContext(RouteContext)
 
   return (
-    <div className="text-white h-[600px] rounded-3xl p-6 font-inter w-[min(90%,_375px)] bg-[#141717]">
+    <motion.div
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      exit={{ scale: 0, opacity: 0 }}
+      transition={{ type: 'tween', duration: 0.2 }}
+      className="text-white h-[600px] rounded-3xl p-6 font-inter w-[min(90%,_375px)] bg-[#141717] origin-bottom ml-auto"
+    >
       {/*TODO abstract routing by creating a route component*/}
       {(route === 'messages' || route === 'support') && <MessagesLayout />}
       {(route === 'enter-pin' || route === 'recover-pin') && <PinRecovery />}
       {route === '/' && <Home />}
       {/* <Suspense fallback={<Loading />}></Suspense> */}
-    </div>
+    </motion.div>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,79 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import detectEthereumProvider from '@metamask/detect-provider'
 
 import logo from '../assets/logo.png'
 import PasswordInput from '../components/PasswordInput'
 import FormButton from '../components/FormButton'
 import CloseWidget from '../components/CloseWidget'
+import RouteContext from '../ContextProvider'
 
 export default function Home() {
-  const [hasProvider, setHasProvider] = useState<boolean | null>(null)
+  const { setIsWidgetVisible } = useContext(RouteContext)
+  const [wallet, setWallet] = useState<{ accounts: string[] }>({ accounts: [] })
+  const [isConnecting, setIsConnecting] = useState(false)
+  const hasWallet = !!wallet.accounts[0]?.length
 
   useEffect(() => {
     const getProvider = async () => {
       const provider = await detectEthereumProvider({ silent: true })
       console.log(provider)
       // Transform provider to true or false.
-      setHasProvider(Boolean(provider))
-    }
+      const hasProvider = Boolean(provider)
 
+      if (!hasProvider) {
+        setIsWidgetVisible(false)
+        return
+      }
+
+      try {
+        // await window.ethereum.request({
+        //   method: 'wallet_revokePermissions',
+        // })
+        setIsConnecting(true)
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+          params: [
+            {
+              chainId: '0xae3f3',
+              chainName: 'Sei Devnet',
+              rpcUrls: [
+                'https://evm-rpc.arctic-1.seinetwork.io',
+                'https://evm-rpc.arctic-1.seinetwork.io',
+              ],
+            },
+          ],
+        })
+        console.log('connected accounts')
+        updateWallet(accounts)
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0xae3f3' }],
+        })
+        console.log('switched chain')
+      } catch (err) {
+        console.log(err.code, err.message)
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error.
+          // If this happens, the user rejected the connection request.
+          setIsWidgetVisible(false)
+          console.log('Please connect to MetaMask.')
+        } else {
+          // console.error(err)
+        }
+      } finally {
+        setIsConnecting(false)
+      }
+    }
     getProvider()
   }, [])
 
+  const updateWallet = async (accounts: any) => {
+    setWallet({ accounts })
+  }
+
   return (
     <div className="grid h-full">
+      {/* {wallet.accounts[0]} */}
       <div>
         <CloseWidget />
         <h1 className="font-semibold text-3xl mt-3 mb-2 capitalize">
@@ -33,8 +85,16 @@ export default function Home() {
           for extra security
         </p>
         <form action="" className="mt-8">
-          <PasswordInput label="enter password" htmlFor="password" />
-          <PasswordInput label="re-enter password" htmlFor="password" />
+          <PasswordInput
+            label="enter password"
+            htmlFor="password"
+            disabled={isConnecting || !hasWallet}
+          />
+          <PasswordInput
+            label="re-enter password"
+            htmlFor="password"
+            disabled={isConnecting || !hasWallet}
+          />
           <div className="flex items-start gap-2 text-sm">
             <p>*</p>
             <p>
@@ -44,7 +104,7 @@ export default function Home() {
           </div>
           <FormButton
             className="mt-8 disabled:cursor-not-allowed"
-            disabled={!hasProvider}
+            disabled={isConnecting || !hasWallet}
           >
             check
           </FormButton>
