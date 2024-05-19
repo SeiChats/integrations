30e3bfb44bb8a029cf53db52ff5ce9509d9e0a2e
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-05-19 09:14:25 +0100

    feat: changed build settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: 'index.js',
+        assetFileNames: assetInfo => {
+          if (assetInfo.name!.endsWith('.css')) {
+            return 'index.css'
+          }
+          return assetInfo.name!
+        },
+      },
+    },
+  },
 })
