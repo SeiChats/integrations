499192bab61f7e3bc9e49ca38a7d4a2cdc4bd88b
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-06-02 13:17:27 +0100

    style: footer icons show active page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
 import inbox from '../assets/inbox.svg'
+import inboxActive from '../assets/inbox-active.svg'
 import sent from '../assets/sent.svg'
+import sentActive from '../assets/sent-active.svg'
 import draft from '../assets/draft.svg'
+import draftActive from '../assets/draft-active.svg'
 import logoWhite from '../assets/logo-white.svg'
 import { twMerge } from 'tailwind-merge'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import RouteContext from '../providers/ContextProvider'
 
 interface FooterProps {
@@ -11,7 +14,26 @@ interface FooterProps {
 }
 
 const Footer = function ({ className }: FooterProps) {
-  const { navigateTo } = useContext(RouteContext)
+  const { navigateTo, route, prevRoute } = useContext(RouteContext)
+  const [activeRoute, setActiveRoute] = useState('inbox')
+
+  useEffect(() => {
+    if (route === 'inbox') {
+      setActiveRoute('inbox')
+    }
+    if (
+      route === 'sent-messages' ||
+      (route.startsWith('sent-message/') && prevRoute === 'sent-messages')
+    ) {
+      setActiveRoute('sent-messages')
+    }
+    if (
+      route === 'drafts' ||
+      (route.startsWith('messages/') && prevRoute === 'drafts')
+    ) {
+      setActiveRoute('drafts')
+    }
+  }, [route])
 
   return (
     <footer
@@ -22,18 +44,30 @@ const Footer = function ({ className }: FooterProps) {
     >
       <div className="w-full h-[1px] bg-white/5 my-4" />
       <div onClick={() => navigateTo('inbox')} className="cursor-pointer">
-        <img src={inbox} alt="inbox" className="mx-auto mb-1" />
+        <img
+          src={activeRoute === 'inbox' ? inboxActive : inbox}
+          alt="inbox"
+          className="mx-auto mb-1"
+        />
         <span className="opacity-80">inbox</span>
       </div>
       <div
         onClick={() => navigateTo('sent-messages')}
         className="cursor-pointer"
       >
-        <img src={sent} alt="sent" className="mx-auto mb-1 mt-1" />
+        <img
+          src={activeRoute === 'sent-messages' ? sentActive : sent}
+          alt="sent"
+          className="mx-auto mb-1 mt-1"
+        />
         <span className="opacity-80">sent</span>
       </div>
       <div onClick={() => navigateTo('drafts')} className="cursor-pointer">
-        <img src={draft} alt="draft" className="mx-auto mb-1 mt-1" />
+        <img
+          src={activeRoute === 'drafts' ? draftActive : draft}
+          alt="draft"
+          className="mx-auto mb-1 mt-1"
+        />
         <span className="opacity-80">draft</span>
       </div>
       <a href="https://app.seichats.com" target="_blank">
